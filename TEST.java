f648e2871430722e9f0361a7b85ec88e20784678
Sherab Tshering Sherpa <156066161+SherabTsheringSherpa@users.noreply.github.com>
2024-05-28 08:09:58 +0530

    Update TEST.java

--- TEST.java
+++ TEST.java
@@ -11,10 +11,7 @@ public class Main {
         System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
     }
 
-    // Function to calculate the sum of two numbers
-    public static int calculateSum(int a, int b) {
-        return a + b;
-    }
+   
 
     // Function to calculate the product of two numbers
     public static int calculateProduct(int a, int b) {
