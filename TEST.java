10a88720b47daa5110401d5f735f388052d3c3be
= <noconstantemails@gmail.com>
2024-05-28 07:58:13 +0530

    first

---
+++ TEST.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = calculateSum(num1, num2);
+        int product = calculateProduct(num1, num2);
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+
+    // Function to calculate the sum of two numbers
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    // Function to calculate the product of two numbers
+    public static int calculateProduct(int a, int b) {
+        return a * b;
+    }
+}
